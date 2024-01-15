c191a68030ec961e12f06d9a51164ac9aa89f9e5
Nathaniel Hartley <nathaniel.hartley@gmail.com>
2024-01-15 23:04:54 +0000

    Document a few constants

--- ecoforest/consts.py
+++ ecoforest/consts.py
@@ -13,3 +13,28 @@ LOCAL_TIMEOUT = httpx.Timeout(
 DIGITAL_READINGS_MAPPINGS = {
     'POOL_ENABLED': 62
 }
+
+ANALOGUE_READINGS_MAPPINGS = {
+
+    # Temperature input brine
+    "TIP_a": 1,
+    # Temperature return brine
+    "TRP_a": 2,
+
+    # Temperature climate input
+    "TIC_a": 4,
+    # Tempereature climate return
+    "TRC_a": 5,
+
+    # Pressure, climate XXX ?
+    "PCP_a": 3,
+    # Pressure, climate XXX ?
+    "PCC_a": 6,
+
+    'energ_util_heat': 133,
+    'energ_util_cool': 134,
+    'ELECTRICITY_USE': 135,
+    'cop_value': 136,
+    'eer_value': 137,
+    'pf_value': 138
+}
